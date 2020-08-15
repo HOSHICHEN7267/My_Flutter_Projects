cad2ab11be792154419d496a563fb5a2bb1262ff
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2020-08-15 10:53:09 +0800

    gsheet version using gscript(not done)

--- road_to_university_2/lib/Uschool.dart
+++ road_to_university_2/lib/Uschool.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'package:gsheets/gsheets.dart';
 import 'Uhome.dart';
 import 'Thome.dart';
 import 'profile.dart';
@@ -32,83 +34,90 @@ class UschoolState extends State<Uschool>{
   final primary = Color(0xff696b9e);
   final secondary = Color(0xfff29a94);
 
+  Future<List<SchoolData>> getSchoolData() async{
+    final url = 'https://script.google.com/macros/s/AKfycbxORWnSA3tmN8Quk6FJmVPdMVVWsHRAPyWS5qFXWjWwC6GNkH4/exec';
+    var datas;
+
+    await http.get(url).then((response){
+      print(response.body);
+      datas = jsonDecode(response.body);
+    });
+
+    List<SchoolData> data = new List<SchoolData>();
+    for(int i = 0 ; i < datas.length ; i++){
+      final now = datas[i];
+      data.add(SchoolData(now[0], now[1], now[2], now[3]));
+    }
+
+    return data;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfff0f0f0),
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 145),
-                //height: MediaQuery.of(context).size.height,
-                width: double.infinity,
-                child: _ListView(context),
-              ),
-              Container(
-                height: 140,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: primary,
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(30),
-                        bottomRight: Radius.circular(30))),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "校名查詢",
-                        style: TextStyle(color: Colors.white, fontSize: 24),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.favorite,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
+      body: FutureBuilder(
+        future: getSchoolData(),
+        builder: (context, snap){
+          if(!snap.hasData){
+            print('hi');
+            return Container();
+          }
+          List<SchoolData> schoolDatas = snap.data;
+          return SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(top: 145),
+                    //height: MediaQuery.of(context).size.height,
+                    width: double.infinity,
+                    child: _ListView(context, schoolDatas),
                   ),
-                ),
-              ),
-              Container(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 110.0,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Material(
-                        elevation: 5.0,
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                        child: Text(
-                          '請點擊想查詢的學校',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black38,
+                  Container(
+                    height: 140,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: primary,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            bottomRight: Radius.circular(30))),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
+                          Text(
+                            "校名查詢",
+                            style: TextStyle(color: Colors.white, fontSize: 24),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.favorite,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: onTabTapped,
@@ -143,114 +152,97 @@ class UschoolState extends State<Uschool>{
     });
   }
 
-  Widget _ListView(BuildContext context){
-    final _Universities = ["國立臺灣大學", "國立臺灣師範大學", "國立中興大學", "國立成功大學", "東吳大學",
-      "國立政治大學", "高雄醫學大學", "中原大學", "東海大學", "國立清華大學",
-      "中國醫藥大學", "國立交通大學", "淡江大學", "逢甲大學", "國立中央大學",
-      "中國文化大學", "靜宜大學", "大同大學", "輔仁大學", "國立臺灣海洋大學",
-      "國立高雄師範大學", "國立彰化師範大學", "國立陽明大學", "中山醫學大學", "國立中山大學",
-      "國立臺北藝術大學", "長庚大學", "國立臺中教育大學", "國立臺北教育大學", "國立臺南大學",
-      "國立東華大學", "臺北市立大學", "國立屏東大學", "國立臺東大學", "國立體育大學",
-      "元智大學", "國立中正大學", "大葉大學", "中華大學", "華梵大學",
-      "義守大學", "銘傳大學", "世新大學", "實踐大學", "長榮大學",
-      "國立臺灣藝術大學", "國立暨南國際大學", "南華大學", "國立臺灣體育運動大學",
-      "國立臺南藝術大學", "玄奘大學", "真理大學", "國立臺北大學", "國立嘉義大學",
-      "國立高雄大學", "慈濟大學", "臺北醫學大學", "開南大學", "台灣首府大學",
-      "康寧大學", "中信金融管理學院", "佛光大學", "明道大學", "亞洲大學",
-      "國立宜蘭大學", "國立聯合大學", "馬偕醫學院", "國立金門大學", "臺北基督學院"];
-
-    final List<String> SchoolCode = ["001", "002", "003", "004", "005", "006", "007", "008", "009", "011",
-      "012", "013", "014", "015", "016", "017", "018", "019", "020", "021",
-      "022", "023", "025", "026", "027", "028", "030", "031", "032", "033",
-      "034", "035", "036", "038", "039", "040", "041", "042", "043", "044",
-      "045", "046", "047", "050", "051", "056", "058", "059", "060",
-      "063", "065", "079", "099", "100", "101", "108", "109", "110", "111",
-      "112", "113", "130", "133", "134", "150", "151", "152", "153", "154"];
-
+  Widget _ListView(BuildContext context, List<SchoolData> schooldatas){
     return ListView.builder(
-      itemCount: _Universities.length,
+      itemCount: schooldatas.length,
       itemBuilder: (context, index){
-        return FlatButton(
-          onPressed: (){
-            Navigator.push(
-              context,
-              PageRouteBuilder(
-                pageBuilder: (context, animation1, animation2) => WebUschool(index: index, SchoolName: _Universities[index],),
-                transitionDuration: Duration(seconds: 0),
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.white,
+          ),
+          width: double.infinity,
+          height: 110,
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 50,
+                height: 50,
+                margin: EdgeInsets.only(right: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(width: 3, color: secondary),
+                  image: DecorationImage(
+                    image: ExactAssetImage('images/university.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
               ),
-            );
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: Colors.white,
-            ),
-            width: double.infinity,
-            height: 110,
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: 50,
-                  height: 50,
-                  margin: EdgeInsets.only(right: 15),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    border: Border.all(width: 3, color: secondary),
-                    image: DecorationImage(
-                      image: ExactAssetImage('images/university.png'),
-                      fit: BoxFit.fill,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      schooldatas[index].name,
+                      style: TextStyle(
+                        color: primary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
                     ),
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        _Universities[index],
-                        style: TextStyle(
-                          color: primary,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22,
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.location_on,
+                          color: secondary,
+                          size: 20,
                         ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.search,
-                            color: secondary,
-                            size: 30,
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          schooldatas[index].address,
+                          style: TextStyle(
+                            color: primary,
+                            fontSize: 13.0,
+                            letterSpacing: .3,
                           ),
-                          SizedBox(
-                            width: 5,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.search,
+                          color: secondary,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          schooldatas[index].num,
+                          style: TextStyle(
+                            color: primary,
+                            fontSize: 13,
+                            letterSpacing: .3,
                           ),
-                          Text(
-                            SchoolCode[index],
-                            style: TextStyle(
-                              color: primary,
-                              fontSize: 20,
-                              letterSpacing: .3,
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       },
@@ -289,32 +281,3 @@ class WebUschoolState extends State<WebUschool>{
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
