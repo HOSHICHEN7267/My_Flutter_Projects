63ec77ce7802a4d8346fa68cd1482466daf82625
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2020-09-11 15:42:26 +0800

    health dairy uploaded

---
+++ health_dairy/android/app/src/main/kotlin/com/example/healthdairy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.healthdairy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ health_dairy/lib/declare.dart
@@ -0,0 +1,23 @@
+class UserProfile{
+  String name;
+  String email;
+  String password;
+  String phone;
+  String line;
+  String weight;
+  String height;
+  String goal;
+}
+
+class DaliyDatas{
+  String actLv;
+  String breakfast;
+  String lunch;
+  String dinner;
+  String other;
+  String water;
+  String exercise;
+  String poo;
+  String hfood;
+  String date;
+}
